e0dd1cb179e3250dcc8b79e7c361cff835a31366
fevin1414 <fevinbiju1414@gmail.com>
2023-09-12 11:30:35 -0500

    added dynamic Api id

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -1,22 +1,40 @@
-import {buildFeedbackPath,extractFeedback} from '../api/feedback'
-function FeedbackPage(props){
-  return <ul>
-{props.feedbackItems.map((item)=>(
-  <li key={item.id}>{item.text}</li>
-))}
-  </ul>
-}
+import { buildFeedbackPath, extractFeedback } from "../api/feedback";
+import { Fragment, useState } from "react";
+function FeedbackPage(props) {
+  const [feedbackData, setFeedbackData] = useState();
 
-export async function getStaticProps(){
-const filePath=buildFeedbackPath();
-const data=extractFeedback(filePath);
-
-return{
-  props:{
-    feedbackItems:data,
+  function loadFeedback(id) {
+    fetch(`/api/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setFeedbackData(data.feedback);
+      });
   }
+  return (
+    <Fragment>
+      {feedbackData && <p>{feedbackData.email}</p>}
+      <ul>
+        {props.feedbackItems.map((item) => (
+          <li key={item.id}>
+            {item.text}{" "}
+            <button onClick={loadFeedback.bind(null, item.id)}>
+              Show Details
+            </button>
+          </li>
+        ))}
+      </ul>
+    </Fragment>
+  );
 }
+export async function getStaticProps() {
+  const filePath = buildFeedbackPath();
+  const data = extractFeedback(filePath);
 
+  return {
+    props: {
+      feedbackItems: data,
+    },
+  };
 }
 
 export default FeedbackPage;
