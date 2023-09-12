31dd7fab07892ec8c676c4ff02ecd7e7b9a52013
fevin1414 <fevinbiju1414@gmail.com>
2023-09-12 08:58:00 -0500

    added form

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,28 @@
+import { useRef } from "react";
 function HomePage() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+  }
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea row="5" type="text" id="feedback" ref={feedbackInput} />
+        </div>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
